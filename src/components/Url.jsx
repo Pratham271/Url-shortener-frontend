d8d91b18adb103b8610f8f2fbacafe099a8bce58
Pratham Chauhan <83640434+Pratham271@users.noreply.github.com>
2024-03-28 16:23:21 +0530

    Update Url.jsx

--- src/components/Url.jsx
+++ src/components/Url.jsx
@@ -40,16 +40,8 @@ const Url = () => {
             <div className='mt-8'>
                 <Input shortenUrl={shortenUrl} isUrlEmpty={isUrlEmpty} onChange={(e)=> setTag(e.target.value)} placeholder={"Try to think of a unique one"} label={"Tag"}/>  
             </div>
-
             <button onClick={shortenUrl} disabled={isUrlEmpty}
             className={`mt-8 w-full  rounded h-10 ${darkMode?"bg-gray-500 text-white hover:bg-gray-400": "bg-gray-700 hover:bg-gray-900 text-white"}`}>Shrink URL</button>
-
-        <h1 className={`text-4xl font-regular ${darkMode?"text-white":"text-black   "}`}>Enter your url</h1>
-        <div className="lg:flex">
-            <Input />  
-            <button onClick={shortenUrl}
-            className={`ml-2 mt-[49px] w-full lg:w-28  rounded h-10 ${darkMode?"bg-white text-gray-900 hover:bg-[#DEDEDE]": "bg-gray-700 hover:bg-gray-900 text-white"}`}>Short URL</button>
-
         </div>
         <div  className={`mt-6 ${darkMode?"text-white":""}`}>
         {loading?<Spinner/>:<a href={shortUrl} target='_blank'>{shortUrl}</a>}
